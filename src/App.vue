0365802874c63b77491eae1ccc408fab1d875665
celestine <celestine0620@163.com>
2022-09-17 16:27:16 +0800

    项目初始化--引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h1>黑马头条</h1>
+    <div>
+      <i class="toutiao toutiao-lishi"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
     <router-view/>
   </div>
 </template>
